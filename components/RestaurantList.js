9ca3c6150b663a26c1d955d7ea8fcdf33a8bf0bf
mustofa adny <adniadni04@gmail.com>
2024-05-03 22:42:39 +0700

    first commit

---
+++ components/RestaurantList.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import RestaurantItem from "./restaurantItem";
+
+function RestaurantList({ restaurants }) {
+  return (
+    <div className="grid">
+      {restaurants.map((restaurant) => (
+        <RestaurantItem key={restaurant.id} {...restaurant} />
+      ))}
+    </div>
+  );
+}
+
+RestaurantList.propTypes = {
+  restaurants: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      pictureId: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default RestaurantList;
